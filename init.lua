b06baef97b77f62fa4643071237bbf1e94ae918b
liudss <liuzw1951@outlook.com>
2026-03-11 22:36:35 +0800

    feat: add treesitter parsers for config files and remove lua/vim

--- init.lua
+++ init.lua
@@ -884,7 +884,7 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = { 'bash', 'c', 'cpp', 'diff', 'html', 'markdown', 'markdown_inline', 'query', 'json', 'jsonc', 'yaml', 'toml', 'dockerfile', 'xml', 'ini', 'git_config', 'gitignore', 'ssh_config' }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
